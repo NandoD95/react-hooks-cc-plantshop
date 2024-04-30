8dbe880adf069475df530136d9a0a951fe02f0c4
Fernando De Jesus <fdejesus95@outlook.com>
2024-04-30 14:21:12 -0400

    completed

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState([])
+  const [searchPlant, setSearchPlant] = useState('')
+
+  const addPlant = (newPlant) => {
+    fetch("http://localhost:6001/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newPlant)
+    })
+      .then((resp) => resp.json())
+      .then((data) => setPlants([...plants, data]))
+  };
+
+  // const handleSearch = (searchTerm) => {
+    const filtered = plants.filter((plant) =>
+      plant.name.toLowerCase().includes(searchPlant.toLowerCase())
+    );
+  // };
+
+  useEffect(()=> {
+    fetch ('http://localhost:6001/plants')
+    .then((resp) => resp.json())
+    .then((data) => setPlants(data))
+  }, [])
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addPlant={addPlant}/>
+      <Search searchPlant={searchPlant} setSearchPlant={setSearchPlant}/>
+      <PlantList plants={filtered}/>
     </main>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Search() {
+function Search({searchPlant, setSearchPlant}) {
+  // const [searchPlant, setSearchPlant] = useState('')
+
+  const handleChange = (e) => {
+    // const changePlant = e.target.value
+    // setSearchPlant(changePlant)
+    setSearchPlant(e.target.value)
+  }
+  console.log(searchPlant)
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
       <input
         type="text"
         id="search"
-        placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
+        value={searchPlant}
+        onChange={handleChange}
       />
     </div>
   );
